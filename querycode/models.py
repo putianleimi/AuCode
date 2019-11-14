dd1634196feb381e8923aa602d8592a11ca61c8d
jimmychen <zhongming.chen@hotmail.com>
2019-11-15 00:10:03 +0800

    deadline pass

--- querycode/models.py
+++ querycode/models.py
@@ -4,3 +4,7 @@ from django.db import models
 class AuCode(models.Model):
     code = models.CharField(max_length=30)
     add_date = models.DateTimeField('date added')
+
+    def __str__(self):
+        return self.code
+
--- querycode/views.py
+++ querycode/views.py
@@ -1,12 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 
 from .models import AuCode
 
 
 def index(request):
+
     return render(request, 'querycode/index.html')
 
 
 def detail(request):
-    return render(request, 'querycode/detail.html')
+    # ac = get_object_or_404(AuCode, code=request.GET.get('name'))
+    ac = request.GET.get('name')
+    error_msg = ''
+    if not ac:
+        error_msg = "You didn't input anything, try again!"
+        return render(request, 'querycode/detail.html', {
+            'error_msg': error_msg,
+        })
+    aucode = AuCode.objects.filter(code=ac)
+
+    if not aucode:
+        error_msg = "Don't match any report,please note the case"
+        return render(request, 'querycode/detail.html', {
+            'error_msg': error_msg,
+
+        })
+
+
+    return render(request, 'querycode/detail.html', {
+        'aucode': aucode,
+
+
+    })
